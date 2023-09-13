326c97602c2e99e41c0092ca6c5a6ae65b0ca8db
Matt Jones <bigboxer23@gmail.com>
2023-09-12 19:19:19 -0500

    init add of code for solar moon

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,22 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
-import Particle from "../Particle";
-import Home2 from "./Home2";
 import Type from "./Type";
+import Home2 from "./Home2";
 
 function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
-        <Particle />
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
+                Welcome to Solar Moon Analytics!{" "}
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                Our platform empowers businesses to efficiently manage their solar energy devices with:
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -35,13 +29,13 @@ function Home() {
                 src={homeLogo}
                 alt="home pic"
                 className="img-fluid"
-                style={{ maxHeight: "450px" }}
+                style={{ maxHeight: "500px", height: "500px" }}
               />
             </Col>
           </Row>
         </Container>
       </Container>
-      <Home2 />
+     <Home2 />
     </section>
   );
 }
