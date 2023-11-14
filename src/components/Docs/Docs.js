993699c85cd3703d44b96711a0ab5c4e318a05f5
Matt Jones <bigboxer23@gmail.com>
2023-11-13 22:58:13 -0600

    remove axios (#15)
    update small paging styling so footer shows properly

--- src/components/Docs/Docs.js
+++ src/components/Docs/Docs.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Docs() {
   return (
-    <div className={"root-page  container min-vh-95"}>
+    <div className={"root-page  container min-vh-88"}>
       <div className={"d-flex ps-5"}>
         <div className={"h3 p-4"}>Coming Soon</div>
       </div>
