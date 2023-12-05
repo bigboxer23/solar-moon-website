2689df1223d0f4826603b5a2c79928a457f73b19
Matt Jones <bigboxer23@gmail.com>
2023-12-05 13:32:15 -0600

    build: remove deployment script, move to infrastructure module (#17)

--- rollback.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-host=blah.com
-user=ec2-user
-destination=blah.com
-
-echo Rolling back $destination
-ssh -i "~/.ssh/Core.pem" -t $user@$host -o StrictHostKeyChecking=no "rm -rf /var/www/$destination/html; mv /var/www/$destination/html-prev /var/www/$destination/html"
-
